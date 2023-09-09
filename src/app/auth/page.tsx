94cea98dcf9912ee2439d5780a3cb2ae9d0fec11
butterbeetle <butterbeetle@naver.com>
2023-09-09 22:38:22 +0900

    Style. Auth Page 스타일링

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,14 +1,71 @@
+"use client";
+import Input from "@/components/Input";
 import Image from "next/image";
+import { useCallback, useState } from "react";
 
 export default function AuthPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover select-none">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <Image src="/images/logo.png" alt="Logo" width={150} height={150} />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full"></div>
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="username"
+                  label="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                value={email}
+              />
+              <Input
+                id="password"
+                label="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12 text-sm text-center ">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                className="text-white ml-1 hover:underline cursor-pointer"
+                onClick={toggleVariant}
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
