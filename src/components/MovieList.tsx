fcf9d1f444b70a2b962140e09b16e0f9ffe0fd8f
butterbeetle <butterbeetle@naver.com>
2023-10-02 23:42:39 +0900

    Docs. 홈화면 호버시 모달창 잠시 보류

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -34,7 +34,9 @@ export default function MovieList({ title, category }: Props) {
     <>
       {movies ? (
         <div className="px-6 md:px-12 my-4">
-          <p className="text-white text-sm font-semibold mb-2">{title}</p>
+          <p className="text-[#E5E5E5] text-[12px] font-semibold mb-2">
+            {title}
+          </p>
           <Swiper
             modules={[Navigation, Pagination, A11y]}
             loop={true}
@@ -51,7 +53,7 @@ export default function MovieList({ title, category }: Props) {
             }}
           >
             {movies.map((movie) => (
-              <SwiperSlide key={movie.id}>
+              <SwiperSlide key={movie.id} className=" relative">
                 <div className="peer relative w-full h-[17vw]  md:h-[13vw] lg:h-[8vw]">
                   <Image
                     className="object-cover"
@@ -62,25 +64,36 @@ export default function MovieList({ title, category }: Props) {
                   />
                 </div>
                 {/* <div
-                  className="bg-red-500 absolute top-0 transition duration-200 z-10
-      delay-300 w-full
-      peer-hover:scale-125 peer-hover:-translate-y-[2vw]
-      peer-hover:opacity-100
-      "
+                  className="transition duration-300 z-[20] absolute top-0 opacity-0
+                            delay-200 w-full
+                            hover:scale-150
+                            hover:opacity-100
+                            drop-shadow-md select-none"
                 >
-                  <div>
+                  <div className="relative w-full h-[17vw]  md:h-[13vw] lg:h-[8vw]">
                     <Image
-                      className="
-          cursor-pointer
-          object-cover
-          transition duration shadow-xl rounded-t-md 
-          w-full h-[12vw]
-        "
+                      className="object-cover rounded-t-md w-full"
                       src={`${tmdbImageURL}/${movie.backdrop_path}`}
                       alt="thumbnail"
-                      width={200}
-                      height={200}
+                      fill
+                      sizes="150"
                     />
+                  </div>
+                  <div className="relative z-[20] bg-zinc-800 p-2 lg:p-4 w-full transition shadow-md rounded-b-md">
+                    <div className="flex flex-row items-center gap-3">
+                      <div
+                        className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex
+                           justify-center items-center transition hover:bg-neutral-300"
+                      >
+                        <PlayIcon size={20} />
+                      </div>
+                    </div>
+                    <p className="text-green-400 font-semibold mt-4">
+                      New <span className="text-white">2023</span>
+                    </p>
+                    <div className="flex flex-row mt-4 gap-2 items-center">
+                      <p className="text-white text-[10px] lg:text-sm">abc</p>
+                    </div>
                   </div>
                 </div> */}
               </SwiperSlide>
