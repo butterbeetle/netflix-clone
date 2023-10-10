9efac5147fe2e6cc60a4430826e06df5848556d4
butterbeetle <butterbeetle@naver.com>
2023-10-11 02:02:30 +0900

    Feat. Modal overview, title genre 클릭한 슬라이드 정보로 표시

--- src/components/PreviewModalInfo.tsx
+++ src/components/PreviewModalInfo.tsx
@@ -2,6 +2,7 @@ import PreviewModalContent from "./PreviewModalContent";
 import PreviewModalCast from "./PreviewModalCast";
 import PreviewModalVideo from "./PreviewModalVideo";
 import { ModalContent } from "@/model/Content";
+import { genreConversionToString } from "@/util/converter";
 
 export default function PreviewModalInfo({
   id,
@@ -13,18 +14,16 @@ export default function PreviewModalInfo({
     <div className="bg-[#181818] px-10 pt-2 pb-8">
       <div className="flex gap-8 mb-12">
         <div className="text-white text-sm flex flex-col gap-2">
-          <p className="text-xl">Title</p>
+          <p className="text-xl">{title}</p>
           <div className="cursor-pointer">
             <p className="text-[#777777] inline mr-1 cursor-default">장르:</p>
-            <p className="text-white inline hover:underline">액션,</p>
-            <p className="text-white inline hover:underline">판타지,</p>
+            {genreConversionToString(genre_ids).map((genre, index) => (
+              <p key={index} className="text-white inline hover:underline">
+                {`${index === genre_ids.length - 1 ? genre : genre + ","}`}
+              </p>
+            ))}
           </div>
-          <span>
-            1956년, 프랑스의 한 성당에서 신부가 끔찍하게 살해당한다. 이 사건을
-            조사하기 위해 파견된 아이린 수녀는 4년 전 자신을 공포에 떨게 했던
-            악마의 기운을 느낀다. 어두운 밤, 계속해서 일어나는 의문의 사건들
-            가운데 충격적인 진실이 드러나는데…
-          </span>
+          <span className="line-clamp-4">{overview}</span>
         </div>
       </div>
       <PreviewModalCast id={id} />
