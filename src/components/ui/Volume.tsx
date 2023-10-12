d11e92611a8c3ad23366f9e88926e0775d9bad7c
butterbeetle <butterbeetle@naver.com>
2023-10-12 23:37:41 +0900

    Chore. Volume 함수 주석 추가

--- src/components/ui/Volume.tsx
+++ src/components/ui/Volume.tsx
@@ -4,6 +4,11 @@ import VolumeUpIcon from "./icons/VolumeUpIcon";
 type Props = {
   isMuted: boolean;
 };
+/**
+ * Mute 여부에따라 Icon 반환하는 함수
+ * @param isMuted Mute인지 아닌지
+ * @returns Mute/Volume Icon
+ */
 export default function Volume({ isMuted }: Props) {
   if (isMuted) return <VolumeMuteIcon />;
   else return <VolumeUpIcon />;
