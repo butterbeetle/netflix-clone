1b00c692c65c0403d62f62545ccb7ddf81e04e4f
butterbeetle <butterbeetle@naver.com>
2023-10-12 23:09:11 +0900

    Feat. Youtube 링크 만드는 함수로 변경

--- src/components/PreviewModalVideo.tsx
+++ src/components/PreviewModalVideo.tsx
@@ -1,4 +1,4 @@
-import { tmdbVideoURL } from "@/service/tmdb";
+import { makeYoutubeLink } from "@/service/tmdb";
 import ReactPlayer from "react-player";
 
 type Props = {
@@ -7,10 +7,13 @@ type Props = {
 
 export default function PreviewModalVideo({ videoKey }: Props) {
   return (
-    <div className="relative w-full aspect-[16/9]">
-      <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-[#181818] to-[#181818]/10"></div>
+    <div className="relative w-full aspect-[16/9] rounded-t-md">
+      <div
+        className="absolute bottom-0 w-full h-1/2 
+      bg-gradient-to-t from-[#181818] to-[#181818]/10"
+      ></div>
       <ReactPlayer
-        url={tmdbVideoURL + videoKey}
+        url={makeYoutubeLink(videoKey)}
         width="100%"
         height="100%"
         playing={true}
--- src/service/tmdb.ts
+++ src/service/tmdb.ts
@@ -1,3 +1,12 @@
 export const tmdbBaseURL = `https://api.themoviedb.org/3`;
 export const tmdbImageURL = `https://image.tmdb.org/t/p`;
-export const tmdbVideoURL = `https://www.youtube.com/watch?v=`;
+export const tmdbVideoURL = `https://www.youtube.com/embed/`;
+
+/**
+ * Youtube Link 를 만들어서 반환해주는 함수
+ * @param videoKey youtube video key
+ * @returns https://www.youtube.com/embed/${videokey}
+ */
+export function makeYoutubeLink(videoKey: string) {
+  return `${tmdbVideoURL}${videoKey}`;
+}
