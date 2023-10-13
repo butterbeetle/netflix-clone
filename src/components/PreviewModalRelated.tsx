f40fc7594f4d9712834b74c061445c2a9519aa09
butterbeetle <butterbeetle@naver.com>
2023-10-14 01:55:50 +0900

    Feat. 관련 영상 없을때 표시

--- src/components/PreviewModalRelated.tsx
+++ src/components/PreviewModalRelated.tsx
@@ -8,54 +8,70 @@ type Props = {
   videoData: any;
 };
 export default function PreviewModalRelated({ videoData }: Props) {
+  console.log(videoData);
   return (
     <div className="mb-12">
       <div className="text-white mb-1">
         <div className="text-2xl mb-5">관련 영상</div>
       </div>
-      <div className="text-[#4d4d4d]">
-        {videoData?.map((video: any) => (
-          <div
-            key={video.id}
-            className="min-h-[10rem] relative cursor-pointer rounded
+      <div className="text-[#4d4d4d] min-h-[10rem]">
+        {videoData?.length > 0 ? (
+          videoData.map((video: any) => (
+            <div
+              key={video.id}
+              className="min-h-[10rem] relative cursor-pointer rounded
       flex items-center gap-4 p-4 box-border group
       border border-x-0
       border-t-[#404040]
       border-b-[#404040]"
-          >
-            <div
-              className="opacity-0
+            >
+              <div
+                className="opacity-0 min-h-full
             group-hover:opacity-100
             absolute w-full h-full bg-black/10 transition top-0 left-0"
-            />
-            <div className="relative aspect-square">
-              <Image
-                className="object-contain w-full aspect-square"
-                src={makeYoutubeThumbnailIURL(video.key)}
-                width={100}
-                height={100}
-                alt="thumbnail"
               />
-              <div
-                className="opacity-0 duration-500 transition-all
+              <div className="relative aspect-square">
+                <Image
+                  className="object-contain w-full aspect-square"
+                  src={
+                    makeYoutubeThumbnailIURL(video.key) ??
+                    `http://via.placeholder.com/300/FFF000/?text=Thumbnail`
+                  }
+                  width={100}
+                  height={100}
+                  alt="thumbnail"
+                />
+                <div
+                  className="opacity-0 duration-500 transition-all
             group-hover:opacity-100 absolute w-full h-full top-0 left-0 flex items-center justify-center"
-              >
-                <div className="text-white rounded-full border-2 border-white ">
-                  <PlayIcon size={35} />
+                >
+                  <div className="text-white rounded-full border-2 border-white ">
+                    <PlayIcon size={35} />
+                  </div>
                 </div>
               </div>
+              <div>
+                <p className="text-[10px] text-[#d2d2d2]">
+                  {engVideoTypeToKR(video.type)}
+                </p>
+                <p className="text-base text-white">{video.name}</p>
+                <p className="text-sm text-[#46d369]">
+                  {publishedFormat(video.published_at)}
+                </p>
+              </div>
             </div>
-            <div>
-              <p className="text-[10px] text-[#d2d2d2]">
-                {engVideoTypeToKR(video.type)}
-              </p>
-              <p className="text-base text-white">{video.name}</p>
-              <p className="text-sm text-[#46d369]">
-                {publishedFormat(video.published_at)}
-              </p>
-            </div>
+          ))
+        ) : (
+          <div
+            className="min-h-[10rem] relative rounded
+      flex items-center gap-4 p-4 box-border group
+      border border-x-0 justify-center
+      border-t-[#404040]
+      border-b-[#404040]"
+          >
+            관련 영상이 없습니다.
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
