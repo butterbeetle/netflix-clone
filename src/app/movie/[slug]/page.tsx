11f64880e54879bbae290fec0ce67aa5267afdbd
butterbeetle <butterbeetle@naver.com>
2023-10-19 01:26:57 +0900

    Fix. 화면 세로로 늘렸을때도 위 빈공간 클릭 시 재생,멈춤 되도록 변경

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -51,95 +51,100 @@ export default function VideoPage() {
   };
   return (
     <FullScreen
-      className="relative w-full h-full flex justify-center items-center flex-col bg-black"
+      className="relative w-full h-full"
       handle={handle}
       onChange={() => fullScreenHandler()}
     >
-      <div className="relative w-full aspect-square select-none">
-        {mount && (
-          <ReactPlayer
-            ref={videoRef}
-            url={"https://www.youtube.com/watch?v=sF61IuL9C0A"}
-            width="100%"
-            height="100%"
-            playing={state.playing}
-            controls={false}
-            muted={state.muted}
-            volume={state.volume}
-            loop={false}
-            onEnded={() => playPauseHandler(false)}
-            onPlay={() => playPauseHandler(true)}
-            onPause={() => playPauseHandler(false)}
-          />
-        )}
-      </div>
       <div
-        className="border-t-orange-600 border-t-2 absolute bottom-0 w-full text-white 
-      py-5 px-3 flex text-3xl justify-between gap-2 z-[3]"
+        onClick={() => playPauseHandler(!state.playing)}
+        className="w-full h-full flex justify-center items-center flex-col bg-black"
       >
-        <div className="flex gap-3">
-          <div
-            onClick={() => playPauseHandler()}
-            className="cursor-pointer hover:scale-[1.2] transition-all"
-          >
-            <PlayPause isPlaying={state.playing} />
-          </div>
-          <div
-            onClick={() => onRewindHandler(true)}
-            className="relative w-fit cursor-pointer hover:scale-[1.2] transition-all"
-          >
-            <AntiClockWiseIcon />
-            <div className="absolute w-full h-full top-0 left-0 text-[10px] flex justify-center items-center">
-              10
-            </div>
-          </div>
-          <div
-            onClick={() => onRewindHandler(false)}
-            className="relative w-fit cursor-pointer hover:scale-[1.2] transition-all"
-          >
-            <ClockWiseIcon />
-            <div className="absolute w-full h-full top-0 left-0 text-[10px] flex justify-center items-center">
-              10
-            </div>
-          </div>
-          <div className="relative group cursor-pointer  transition-all">
-            <div
-              onClick={() => onMuteHandler()}
-              className="group-hover:scale-[1.2]"
-            >
-              <Volume isMuted={state.muted} />
-            </div>
-            <div className=" bg-[#191919] p-1 scale-[0.8] flex justify-center absolute -top-[70px] -left-14 opacity-0 group-hover:opacity-100 -rotate-90">
-              <input
-                type="range"
-                min={0}
-                max={1}
-                step={0.1}
-                value={state.muted ? 0 : state.volume}
-                onChange={(e) =>
-                  setState({
-                    ...state,
-                    muted: state.volume === 0.1 ? true : false,
-                    volume: Number(e.target.value),
-                  })
-                }
-              />
-            </div>
-          </div>
+        <div className="relative w-full aspect-square select-none">
+          {mount && (
+            <ReactPlayer
+              ref={videoRef}
+              url={"https://www.youtube.com/watch?v=sF61IuL9C0A"}
+              width="100%"
+              height="100%"
+              playing={state.playing}
+              controls={false}
+              muted={state.muted}
+              volume={state.volume}
+              loop={false}
+              onEnded={() => playPauseHandler(false)}
+              onPlay={() => playPauseHandler(true)}
+              onPause={() => playPauseHandler(false)}
+            />
+          )}
         </div>
-        <div className="text-base flex items-center">Title</div>
-        <div className="flex gap-3">
-          <div className="cursor-pointer hover:scale-[1.2] transition-all">
-            <PlayerSkipForwardIcon />
+        <div
+          className="border-t-orange-600 border-t-2 absolute bottom-0 w-full text-white 
+      py-5 px-3 flex text-3xl justify-between gap-2 z-[3]"
+        >
+          <div className="flex gap-3">
+            <div
+              onClick={() => playPauseHandler()}
+              className="cursor-pointer hover:scale-[1.2] transition-all"
+            >
+              <PlayPause isPlaying={state.playing} />
+            </div>
+            <div
+              onClick={() => onRewindHandler(true)}
+              className="relative w-fit cursor-pointer hover:scale-[1.2] transition-all"
+            >
+              <AntiClockWiseIcon />
+              <div className="absolute w-full h-full top-0 left-0 text-[10px] flex justify-center items-center">
+                10
+              </div>
+            </div>
+            <div
+              onClick={() => onRewindHandler(false)}
+              className="relative w-fit cursor-pointer hover:scale-[1.2] transition-all"
+            >
+              <ClockWiseIcon />
+              <div className="absolute w-full h-full top-0 left-0 text-[10px] flex justify-center items-center">
+                10
+              </div>
+            </div>
+            <div className="relative group cursor-pointer  transition-all">
+              <div
+                onClick={() => onMuteHandler()}
+                className="group-hover:scale-[1.2]"
+              >
+                <Volume isMuted={state.muted} />
+              </div>
+              <div className=" bg-[#191919] p-1 scale-[0.8] flex justify-center absolute -top-[70px] -left-14 opacity-0 group-hover:opacity-100 -rotate-90">
+                <input
+                  type="range"
+                  min={0}
+                  max={1}
+                  step={0.1}
+                  value={state.muted ? 0 : state.volume}
+                  onChange={(e) =>
+                    setState({
+                      ...state,
+                      muted: state.volume === 0.1 ? true : false,
+                      volume: Number(e.target.value),
+                    })
+                  }
+                />
+              </div>
+            </div>
           </div>
-          <div className="cursor-pointer hover:scale-[1.2] transition-all">
-            <SquareStackIcon />
-          </div>
-          <div
-            onClick={state.fullScreen ? handle.enter : handle.exit}
-            className="cursor-pointer hover:scale-[1.2] transition-all"
-          >
-            {state.fullScreen ? <FullScreenIcon /> : <FullScreenExitIcon />}
+          <div className="text-base flex items-center">Title</div>
+          <div className="flex gap-3">
+            <div className="cursor-pointer hover:scale-[1.2] transition-all">
+              <PlayerSkipForwardIcon />
+            </div>
+            <div className="cursor-pointer hover:scale-[1.2] transition-all">
+              <SquareStackIcon />
+            </div>
+            <div
+              onClick={state.fullScreen ? handle.enter : handle.exit}
+              className="cursor-pointer hover:scale-[1.2] transition-all"
+            >
+              {state.fullScreen ? <FullScreenIcon /> : <FullScreenExitIcon />}
+            </div>
           </div>
         </div>
       </div>
