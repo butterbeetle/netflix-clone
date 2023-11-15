95bf3146f4a81db0579c8b7bada613f75ddbe081
butterbeetle <butterbeetle@naver.com>
2023-11-15 23:40:44 +0900

    Style. 로그인창 footer 변경

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -58,14 +58,14 @@ export default function AuthPage() {
 
   return (
     <div
-      className="relative w-full h-screen bg-[url('/images/hero.jpg')] 
+      className="relative w-full h-screen md:h-full bg-[url('/images/hero.jpg')] 
     bg-no-repeat  bg-fixed bg-cover select-none"
     >
       <div className="bg-black w-full h-full md:bg-opacity-50">
         <nav className="px-12 py-5">
           <Image src="/images/logo.png" alt="Logo" width={150} height={150} />
         </nav>
-        <div className="flex justify-center mb-0 md:mb-44 border-b border-b-[#737373] md:border-0">
+        <main className="flex justify-center mb-0 md:mb-44 border-b border-b-[#737373] md:border-0">
           <div className="bg-black/80 min-h-[550px] md:min-h-[660px] p-16 self-center mt-2 lg:w-2/5 md:max-w-md rounded-md w-full">
             <h1 className="text-white text-3xl mb-8 font-semibold">
               {variant === "login" ? "로그인" : "회원가입"}
@@ -134,20 +134,22 @@ export default function AuthPage() {
               </span>
             </p>
           </div>
-        </div>
-        <div className="relative  bg-black md:bg-opacity-50 text-[#737373] p-8">
-          <p className="mb-8 ">
-            질문이 있으신가요? 문의 전화: 00-308-321-0161 (수신자 부담)
-          </p>
-          <ul className="text-sm flex flex-wrap w-full">
-            {footerMenu.map(({ title }) => (
-              <li className="w-1/3 mb-4" key={title}>
-                {title}
-              </li>
-            ))}
-          </ul>
-          <FooterInfo />
-        </div>
+        </main>
+        <footer className="relative  bg-black md:bg-opacity-70 text-[#737373] p-8 pb-16">
+          <div className="mx-auto max-w-[1000px]">
+            <p className="mb-8 ">
+              질문이 있으신가요? 문의 전화: 00-308-321-0161 (수신자 부담)
+            </p>
+            <ul className="text-sm flex flex-wrap w-full">
+              {footerMenu.map(({ title }) => (
+                <li className="w-1/3 md:w-1/4 mb-4" key={title}>
+                  {title}
+                </li>
+              ))}
+            </ul>
+            <FooterInfo />
+          </div>
+        </footer>
       </div>
     </div>
   );
